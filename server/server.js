b9f566d4bff6cbd072b71cb9a5d9827105882c2d
赵丁立 <zhaodingli@nsfocus.com>
2024-01-17 10:38:37 +0800

    feat 解析文件成功，1、使用multer中间件，2、formdata.append第一个参数必须和upload.single参数相同

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const multer = require("multer");
 const app = express();
 const port = 6000;
 
-app.post("/api/thumbnail-upload", (req, res) => {
-  console.log("req", req);
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post("/api/thumbnail-upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  console.log("file", file);
   res.send("Hello from Express server");
 });
 
