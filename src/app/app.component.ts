b85b47858ad25433ffc5feb783ce8fcafe4faf45
赵丁立 <zhaodingli@nsfocus.com>
2024-01-19 08:56:51 +0800

    feat mat组件使用成功，即ng add指令

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FileUploadComponent, HttpClientModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FileUploadComponent,
+    HttpClientModule,
+    MatProgressBarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
