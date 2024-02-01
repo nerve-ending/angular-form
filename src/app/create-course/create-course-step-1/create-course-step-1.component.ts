fe34b400a3ca81650354a1570a6300d6ff0fbb08
赵丁立 <zhaodingli@nsfocus.com>
2024-02-01 15:00:17 +0800

    feat 增加一个select，数据从后端拿。1、新建一个流变量（以‘$’结尾）使用服务请求接口存储数据。2、使用ng-container在select内部用作option的循环，注意使用流数据时要使用pipe（async）

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -12,6 +12,13 @@ import { courseTitleValidator } from '../../validators/course-title.validator';
 import { CoursesService } from '../../validators/courses.service';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { Observable } from 'rxjs';
+
+interface CourseCategory {
+  code: string;
+  description: string;
+}
 
 @Component({
   selector: 'create-course-step-1',
@@ -24,12 +31,17 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatInputModule,
     MatDatepickerModule,
     MatCheckboxModule,
+    MatSelectModule,
   ],
   providers: [CoursesService], //解决了ERROR NullInjectorError问题
   templateUrl: './create-course-step-1.component.html',
   styleUrls: ['./create-course-step-1.component.scss'],
 })
 export class CreateCourseStep1Component implements OnInit {
+  courseCategories$: Observable<CourseCategory[]> = new Observable<
+    CourseCategory[]
+  >();
+
   form = this.fb.group({
     title: [
       '',
@@ -44,12 +56,15 @@ export class CreateCourseStep1Component implements OnInit {
       },
     ],
     releasedAt: [new Date(), [Validators.required]],
+    category: ['BEGINNER', [Validators.required]],
     downloadsAllowed: [false, [Validators.requiredTrue]],
     longDescription: ['', [Validators.required, Validators.minLength(3)]],
   });
 
   constructor(private fb: FormBuilder, private courses: CoursesService) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.courseCategories$ = this.courses.findCourseCategories();
+  }
 
   get courseTitle() {
     return this.form.controls['title'];
