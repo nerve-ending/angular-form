d1448ab7d863c9bfc2c4ef8cf2e91991b89a1f48
赵丁立 <zhaodingli@nsfocus.com>
2024-02-01 09:19:39 +0800

    feat node服务器接口完善，熟悉commonjs和esmodule知识

---
+++ server/server.ts
@@ -0,0 +1,37 @@
+// import express from 'express';
+// import { getAllCourses, getCourseById } from './get-courses.route';
+// import { searchLessons } from './search-lessons.route';
+// import { getCourseCategories } from './course-categories.route';
+// import multer from 'multer';
+
+const express = require('express');
+const { getAllCourses, getCourseById } = require('./get-courses.route');
+const { searchLessons } = require('./search-lessons.route');
+const { getCourseCategories } = require('./course-categories.route');
+const multer = require('multer');
+
+const app = express();
+const port = 6000;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.route('/api/courses').get(getAllCourses);
+
+app.route('/api/courses/:id').get(getCourseById);
+
+app.route('/api/lessons').get(searchLessons);
+
+app.route('/api/course-categories').get(getCourseCategories);
+
+app.post(
+  '/api/thumbnail-upload',
+  upload.single('file'),
+  (req: any, res: any) => {
+    res.send('Hello from Express server');
+  }
+);
+
+app.listen(port, () => {
+  console.log('server is running at 6000');
+});
