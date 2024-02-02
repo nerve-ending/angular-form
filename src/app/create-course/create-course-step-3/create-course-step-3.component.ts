a79a23dba79bb417a7df9dc04007a534857a54c5
赵丁立 <zhaodingli@nsfocus.com>
2024-02-02 11:18:18 +0800

    feat 通过formArray设置input组合框的增删

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,22 +1,69 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
+  FormArray,
   FormBuilder,
-  Validators,
+  FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'create-course-step-3',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    MatProgressBarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
   templateUrl: './create-course-step-3.component.html',
   styleUrls: ['./create-course-step-3.component.scss'],
 })
-export class CreateCourseStep3Component {
-  form = this.fb.group({});
+export class CreateCourseStep3Component implements OnInit {
+  form = this.fb.group({
+    lessons: this.fb.array([]),
+  });
 
   constructor(private fb: FormBuilder) {}
+
+  get lessons() {
+    return this.form.controls['lessons'] as FormArray;
+  }
+
+  addLessons() {
+    const lessonForm = this.fb.group({
+      title: ['', Validators.required],
+      level: ['beginner', Validators.required],
+    });
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(lessonIndex: number) {
+    this.lessons.removeAt(lessonIndex);
+  }
+
+  ngOnInit(): void {}
 }
