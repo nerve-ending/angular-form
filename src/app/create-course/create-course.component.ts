02c6beeeda79450a660bdee87ff7b42bedee6d2a
赵丁立 <zhaodingli@nsfocus.com>
2024-02-02 11:39:12 +0800

    feat 注释掉调试内容，基本功能完成

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -31,6 +31,6 @@ export class CreateCourseComponent implements OnInit {
   ngOnInit(): void {}
 
   submit(step1: any, step2: any, step3: any) {
-    console.log('step1', step1);
+    console.log(step1, step2, step3);
   }
 }
